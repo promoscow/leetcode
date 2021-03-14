63e8d9ad2a97e2efe548392cb2895ccb72500f3e
Vyacheslav Chernyshoff <slava_rossii@list.ru>
2021-03-14 21:36:11 +0300

    + https://leetcode.com/problems/range-sum-of-bst/

--- src/ru/xpendence/Solution.java
+++ src/ru/xpendence/Solution.java
@@ -1,6 +1,5 @@
 package ru.xpendence;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -31,7 +30,7 @@ public class Solution {
     }
 
     public int[] runningSum(int[] nums) {
-        var result =new int[nums.length];
+        var result = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             if (i == 0) {
                 result[i] = nums[i];
@@ -44,8 +43,8 @@ public class Solution {
 
     public int uniqueMorseRepresentations(String[] words) {
         var morseAlphabet = new String[]{
-                ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",
-                ".-.","...","-","..-","...-",".--","-..-","-.--","--.."
+                ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-",
+                ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
         };
         var morseMap = new HashMap<Character, String>();
         for (int i = 0; i < 26; i++) {
@@ -66,10 +65,58 @@ public class Solution {
                 .count();
     }
 
-    
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        return processNode(root, low, high);
+    }
+
+    private int processNode(TreeNode node, int low, int high) {
+        var sum = 0;
+        if (node != null) {
+            if (node.val >= low && node.val <= high) {
+                sum += node.val;
+                sum += processNode(node.left, low, high);
+                sum += processNode(node.right, low, high);
+            } else if (node.val < low) {
+                sum += processNode(node.right, low, high);
+            } else {
+                sum += processNode(node.left, low, high);
+            }
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
         var solution = new Solution();
-        System.out.println(solution.uniqueMorseRepresentations(new String[]{"gin", "zen", "gig", "msg"}));
+        var n1 = new TreeNode(1);
+        var n3 = new TreeNode(3, n1, null);
+        var n6 = new TreeNode(6);
+        var n7 = new TreeNode(7, n6, null);
+        var n5 = new TreeNode(5, n3, n7);
+        var n18 = new TreeNode(18);
+        var n13 = new TreeNode(13);
+        var n15 = new TreeNode(15, n13, n18);
+        var n10 = new TreeNode(10, n5, n15);
+
+        System.out.println(solution.rangeSumBST(n10, 6, 10));
+    }
+
+    //Definition for a binary tree node.
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
